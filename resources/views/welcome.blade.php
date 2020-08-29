ce662f7437c1226316c01801385f898a9ee5df74
Hung LV <hunglv@vnext.com.vn>
2020-08-29 21:12:13 +0700

    update home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel version 7.26.1
                 </div>
 
                 <div class="links">
