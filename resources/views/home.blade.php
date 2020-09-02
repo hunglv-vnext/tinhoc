d725396e38ca593e27625f06cc77f2bd4d12552e
Hung LV <hunglv@vnext.com.vn>
2020-09-02 14:59:49 +0700

    edit view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! hunglv') }}
                 </div>
             </div>
         </div>
